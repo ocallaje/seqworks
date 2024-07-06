4975f745698809cb74d6d830e384fbb90412e54c
ocallaje <ocallaje@tcd.ie>
2024-07-06 18:16:04 +0100

    undo reflow

--- src/app-wide.js
+++ src/app-wide.js
@@ -51,12 +51,6 @@
   document.addEventListener('DOMContentLoaded', function() {
     loadTemplate('sidebar.html', 'sidebarContent');
     loadTemplate('topbar.html', 'topbarContent', setTodaysDate);
-
-    setTimeout(() => {
-      window.dispatchEvent(new Event('resize'));
-    }, 100);
-    console.log("reflow")
-
   });
   
   // Function to load a template
